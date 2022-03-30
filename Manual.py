4caabf946d69abd50de9645cefc0eca62553e85f
Raz Elbaz <93310416+RazElbaz@users.noreply.github.com>
2022-03-30 11:14:14 +0300

    Add files via upload

--- Manual.py
+++ Manual.py
@@ -25,7 +25,7 @@ def Transfer_to_Jason(start_check, end_check):
     new_process = dict()
     nonexistent_process = dict()
     # timedelta represent the difference between two datetime objects.
-    time_delta = timedelta(seconds=0.5)
+    time_delta = timedelta(seconds=1)
 
     # The strptime() function converts the character string pointed to by buf to values which are stored in the tm
     # structure pointed to by tm, using the format specified by format syntax is:time.strptime(time_string[, format])
@@ -56,7 +56,7 @@ def Transfer_to_Jason(start_check, end_check):
                 flag2 = 1
     # The flags were designed to ensure that only the 2 most accurate samples were taken
     if flag1 == 0:
-        print("There is no new process")
+        print("There is no new process ")
     if flag2 == 0:
         print("There is no nonexistent process")
 
